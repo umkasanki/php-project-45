63fb972c542c46ebabcded1d4f1d4839d961c929
Oleg Tishkin <umkasanki@gmail.com>
2024-02-03 20:28:00 +0300

    add calc logic

--- src/Calc.php
+++ src/Calc.php
@@ -2,32 +2,33 @@
 
 namespace BrainGames\Calc;
 
+use function BrainGames\Cli\isEven;
 use function cli\line;
 use function cli\prompt;
 
-function operation() :string
+function operation(): string
 {
     $operations = ['+', '-', '*'];
     $currentOperationKey = array_rand($operations);
     return $operations[$currentOperationKey];
 }
 
-function sum(int $operandOne, int $operandTwo) :int
+function sum(int $operandOne, int $operandTwo): int
 {
     return $operandOne + $operandTwo;
 }
 
-function sub(int $operandOne, int $operandTwo) :int
+function sub(int $operandOne, int $operandTwo): int
 {
     return $operandOne - $operandTwo;
 }
 
-function multiply(int $operandOne, int $operandTwo) :int
+function multiply(int $operandOne, int $operandTwo): int
 {
     return $operandOne * $operandTwo;
 }
 
-function answer(): string
+function getAnswer(): string
 {
     $operandOne = rand(0, 9);
     $operandTwo = rand(0, 9);
@@ -35,5 +36,39 @@ function answer(): string
 
     line("Question: $operandOne $operator $operandTwo");
 
-    return prompt('Your answer');
+    $operatorResult = null;
+
+    switch ($operator) {
+        case '+':
+            $operatorResult = $operandOne + $operandTwo;
+            break;
+        case '-':
+            $operatorResult = $operandOne - $operandTwo;
+            break;
+        case '*':
+            $operatorResult = $operandOne * $operandTwo;
+            break;
+    }
+
+    $answer = intval(prompt('Your answer'));
+
+    return $operatorResult === $answer;
+}
+
+function calc(string $name)
+{
+    $i = 0;
+    while ($i < 3) {
+        if (getAnswer()) {
+            print_r("Correct!\n");
+        } else {
+            break;
+        }
+
+        $i = $i + 1;
+
+        if ($i === 3) {
+            line("Congratulations, $name!");
+        }
+    }
 }
