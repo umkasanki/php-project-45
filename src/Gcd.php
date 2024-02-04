a08127eec2aff6fda54dcf4e08208cb975576603
Oleg Tishkin <umkasanki@gmail.com>
2024-02-04 18:05:04 +0300

    refactor bran-gcd script

--- src/Gcd.php
+++ src/Gcd.php
@@ -22,30 +22,3 @@ function getRandomNumbers(): array
     return [rand(2, 20), rand(2, 20)];
 }
 
-function askQuestion(): bool
-{
-    [$x, $y] = getRandomNumbers();
-
-    line("Question: $x $y");
-    $answer = intval(prompt('Your answer'));
-
-    return $answer === gcd($x, $y);
-}
-
-function runQuiz(string $name): void
-{
-    $i = 0;
-    while ($i < 3) {
-        if (askQuestion()) {
-            print_r("Correct!\n");
-        } else {
-            break;
-        }
-
-        $i = $i + 1;
-
-        if ($i === 3) {
-            line("Congratulations, $name!");
-        }
-    }
-}
