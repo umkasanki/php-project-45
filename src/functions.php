6536490c88bc236e4145ee6c6ab152df7dbeb788
Oleg Tishkin <umkasanki@gmail.com>
2024-02-25 16:01:12 +0300

    add string logic

--- src/functions.php
+++ src/functions.php
@@ -2,33 +2,54 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-function barChart($arr):void
+function barChart($arr):string
 {
     if (empty($arr)) {
-        return;
+        return '';
     }
 
     $min = min(min($arr), 0);
     $max = max(max($arr), 0);
 
+    $out = '';
+
     for ($row = $max; $row > $min; $row--) {
         foreach ($arr as $value) {
             if ($row > 0) {
                 if ($value >= $row) {
-                    echo '*';
+                    $out .=  '*';
                 } else {
-                    echo ' ';
+                    $out .=  ' ';
                 }
             } else {
                 if ($value < $row) {
-                    echo '# ';
+                    $out .=  '# ';
                 } else {
-                    echo ' ';
+                    $out .=  ' ';
                 }
             }
         }
-        echo "\n";
+        if ($row != $min + 1) {
+            $out .=  "\n";
+        }
     }
+    return $out;
 }
 
-barChart([5, 10, -5, -3, 7]);
+$data = [
+    ' *   ',
+    ' *  *',
+    ' *  *',
+    ' *  *',
+    '**  *',
+    '  #  ',
+    '  #  ',
+    '  #  '
+];
+$expected = implode("\n", $data);
+
+$numbers = [1, 5, -3, 0, 4];
+//$numbers = [-5, 5, -3, -2, -1, 1, 2, 3, 0, 6];
+
+//var_dump(barChart($numbers));
+var_dump($expected);
